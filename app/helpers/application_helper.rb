04fb56cdac4f5d9d12040be572573faf6dda8874
Marigi <marigindegwa@gmail.com>
2020-12-18 23:58:01 +0300

    Added Views and Pricing Page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+def current_user_subscribed?
+  user_signed_in? && current_user.subscribed?
+end
+
+def admin?
+  user_signed_in? && current_user.admin?
+end
+
+def subscribed?
+  user_signed_in? && current_user_subscribed?
+end
+
+def titlize?
+  str.gsub('_','').capitalize
+end
+
 end
